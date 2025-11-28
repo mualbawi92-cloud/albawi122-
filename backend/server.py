fc502fc79b238ddcd7b94018c8805a11cc8ed752
mualbawi92-cloud <mualbawi92@gmail.com>
2025-11-28 16:04:36 +0000

    auto-commit for 26bba73d-1f57-43f5-acbb-c5c7b6ae9d8a

--- backend/server.py
+++ backend/server.py
@@ -1433,7 +1433,7 @@ async def create_transfer(transfer_data: TransferCreate, current_user: dict = De
             )
             
             await db.chart_of_accounts.update_one(
-                {'code': '203'},
+                {'code': '901'},
                 {'$inc': {'balance': -transfer_data.amount, 'balance_iqd': -transfer_data.amount}}
             )
             
