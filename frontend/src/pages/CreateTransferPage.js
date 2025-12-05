80b83ea1fb36d355b04d3dada0e0f89d63dd0d39
mualbawi92-cloud <mualbawi92@gmail.com>
2025-12-05 20:40:24 +0000

    auto-commit for 98d0e372-e716-46db-954e-eb5e55b3aaa5

--- frontend/src/pages/CreateTransferPage.js
+++ frontend/src/pages/CreateTransferPage.js
@@ -361,23 +361,27 @@ const CreateTransferPage = () => {
           <table class="main-table">
             <tr>
               <td class="label-col">اسم المرسل</td>
-              <td class="value-col">${result.sender_name}</td>
+              <td class="value-col">${result.sender_name || ''}</td>
             </tr>
+            ${result.sender_phone ? `
             <tr>
               <td class="label-col">رقم هاتف المرسل</td>
-              <td class="value-col">${result.sender_phone || 'غير متوفر'}</td>
+              <td class="value-col">${result.sender_phone}</td>
             </tr>
+            ` : ''}
             <tr>
               <td class="label-col">اسم المستلم</td>
-              <td class="value-col">${result.receiver_name}</td>
+              <td class="value-col">${result.receiver_name || ''}</td>
             </tr>
+            ${result.receiver_phone ? `
             <tr>
               <td class="label-col">رقم هاتف المستلم</td>
-              <td class="value-col">${result.receiver_phone || 'غير متوفر'}</td>
+              <td class="value-col">${result.receiver_phone}</td>
             </tr>
+            ` : ''}
             <tr>
               <td class="label-col">المحافظة</td>
-              <td class="value-col">${result.to_governorate}</td>
+              <td class="value-col">${result.to_governorate || ''}</td>
             </tr>
             ${result.to_agent_name ? `
             <tr>
