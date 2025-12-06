328fbb86363d5cd507dfc70dd8ad0b845321d44e
mualbawi92-cloud <mualbawi92@gmail.com>
2025-12-06 18:49:00 +0000

    auto-commit for 331a5b9f-1284-45c8-913b-41dd2bec3f02

--- frontend/src/pages/CreateTransferPage.js
+++ frontend/src/pages/CreateTransferPage.js
@@ -11,6 +11,7 @@ import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, D
 import { toast } from 'sonner';
 import Navbar from '../components/Navbar';
 import { formatAmountInWords } from '../utils/arabicNumbers';
+import { generateHTMLFromVisualTemplate } from '../utils/printUtils';
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const API = `${BACKEND_URL}/api`;
